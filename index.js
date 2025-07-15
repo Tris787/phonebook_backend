77d13709fe1acab33abb2ad7719f5dc343d489db
Tris787 <thomas_kuemmet@gmx.de>
2025-07-15 12:52:16 +0200

    after 3.14

--- index.js
+++ index.js
@@ -51,26 +51,18 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const personExists = persons.find(person => person.name === body.name)
-  if (personExists) {
-    return response.status(400).json({
-      error: 'name must be unique'
-    })
-  }  
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: String(Math.floor(Math.random()*1000) +1),
-  }
+  })
 
-  persons = persons.concat(person)
-
-  response.json(person)
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
 })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`)
 })
-
