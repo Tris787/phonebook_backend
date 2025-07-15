67d590eb29f1bcc4c5197748954d52462b069647
Tris787 <thomas_kuemmet@gmx.de>
2025-07-15 14:13:56 +0200

    after 3.15

--- index.js
+++ index.js
@@ -36,10 +36,14 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 app.post('/api/persons', (request, response) => {
