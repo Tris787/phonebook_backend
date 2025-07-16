a63ac489388ce18e6ef1c12f9f9d98bcb02d2308
Tris787 <thomas_kuemmet@gmx.de>
2025-07-16 10:51:18 +0200

    after 3.22

--- index.js
+++ index.js
@@ -9,23 +9,21 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
 app.use(express.static('dist'))
 
-let persons = []
-
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(person => {
-  console.log(person)
-  response.json(person)
+    console.log(person)
+    response.json(person)
   })
 })
 
-app.get('/info/', (request, response) => {
+app.get('/info/', (request, response, next) => {
   const requestTime = new Date()
   Person.countDocuments({})
     .then(number => {
       response.send(`<p>Phonebook has info for ${number} people</p>
                      <p>${requestTime.toLocaleString()}</p>`)
     })
-    .catch(error => next(error))  
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -40,9 +38,9 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -50,12 +48,6 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
-
-  /*if (!(body.name && body.number)) {
-    return response.status(400).json({
-      error: 'name or number missing'
-    })
-  }*/
 
   const person = new Person({
     name: body.name,
@@ -69,7 +61,7 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
+app.put('/api/persons/:id', (request, response, next) => {
   const { name, number } = request.body
   Person.findById(request.params.id)
     .then(person => {
--- models/person.js
+++ models/person.js
@@ -7,7 +7,7 @@ const url = process.env.MONGODB_URI
 console.log('connection to', url)
 mongoose
   .connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -26,7 +26,7 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     validate: {
       validator: v => {
-        return /^\d{2,3}-\d+$/.test(v);
+        return /^\d{2,3}-\d+$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
